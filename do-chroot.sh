3b4a86b63a8372ccd055c53c096c094d585d4307
Cerem Cem ASLAN <ceremcem@aktos.io>
2019-12-23 12:24:35 +0000

    mount /run to the $target_root

--- do-chroot.sh
+++ do-chroot.sh
@@ -17,13 +17,18 @@ echo chrooting into $1
 mkdir -p $1/proc
 mkdir -p $1/sys
 mkdir -p $1/dev
+mkdir -p $1/run
 
-cp /etc/resolv.conf $1/etc
+cat << RESOLV > $1/etc/resolv.conf
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+RESOLV
 
 mount --bind /proc $1/proc
 mount --bind /sys $1/sys
 mount --bind /dev $1/dev
 mount --bind /dev/pts $1/dev/pts
+mount --bind /run $1/run
 
 chroot $1 /bin/bash
 
@@ -32,8 +37,8 @@ umount $1/dev/pts
 umount $1/dev
 umount $1/sys
 umount $1/proc || umount -lf $1/proc
+umount $1/run
 
-rm $1/etc/resolv.conf 2> /dev/null
 rm $1/var/lib/dbus/machine-id 2> /dev/null
 
 echo "Done."
