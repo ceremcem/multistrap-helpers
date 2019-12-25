f8b141f203fc0914e36d8a354333b4823ec92d05
Cerem Cem ASLAN <ceremcem@aktos.io>
2019-12-25 03:27:05 +0000

    rsync-to-disk generates /install-grub.sh script

--- install-to-disk/rsync-to-disk.sh
+++ install-to-disk/rsync-to-disk.sh
@@ -5,9 +5,18 @@ safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_S
 
 safe_source $_sdir/config.sh
 
-if [[ -z ${src:-} ]]; then
+if [[ -z ${1:-} ]]; then
 	echo "Usage: $(basename $0) path/to/your-rootfs"
 	exit 1
 fi
+src="$1"
 
 rsync -avP $src/ $rootfs_mnt
+
+cat << INSTALL_GRUB > $rootfs_mnt/install-grub.sh
+boot_part_dev=\$(blkid | grep ${boot_part##UUID=} | cut -d: -f1)
+disk_device=\${boot_part_dev::-1}
+grub-install \$disk_device --boot-directory=/boot
+INSTALL_GRUB
+
+chmod +x $rootfs_mnt/install-grub.sh
