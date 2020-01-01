e69b5120f12c4b72441536b5855542e89d9591f5
Cerem Cem ASLAN <cem@aktos.io>
2020-01-01 12:26:54 +0300

    added RAID1 degraded mode tip

--- install-to-disk/attach-disk.sh
+++ install-to-disk/attach-disk.sh
@@ -5,6 +5,11 @@ safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_S
 
 safe_source $_sdir/config.sh
 
+function echo_and_run {
+  echo "$@"
+  eval $(printf '%q ' "$@") < /dev/tty
+}
+
 [[ $(whoami) = "root" ]] || { sudo "$0" "$@"; exit 0; }
 
 # Attach the disk
@@ -18,7 +23,23 @@ sleep 2
 umount $boot_part 2> /dev/null || true
 umount $root_dev 2> /dev/null || true
 
+set +e
 mount $root_dev $root_mnt # for subvolume operations
+if [[ $? -ne 0 ]]; then
+    cat << EOL
+
+Mounting $root_dev failed. If this device had a RAID-1
+configuration and the failure is because of a missing device,
+you may try to mount the partition Readonly by the following
+command:
+
+	mount -t btrfs -o ro,degraded $root_dev $root_mnt
+
+EOL
+    exit
+fi
+set -e
+
 [[ -d $root_mnt/$subvol ]] || btrfs sub create $root_mnt/$subvol
 mount -t btrfs -o subvol=$subvol $root_dev $rootfs_mnt
 
