79192ca3e60afa2476af0832f72da25d694ecc3b
Cerem Cem ASLAN <cem@aktos.io>
2020-12-25 22:58:51 +0300

    improved documentation

--- install-to-disk/config-example.sh
+++ install-to-disk/config-example.sh
@@ -1,14 +1,16 @@
-# Add info to the configuration file:
-#
-#     ./get-disk-info.sh /dev/sdc | sed -e 's/^/# /' | tee -a config-zencefil.sh
+# Identify your disk (like UUID of a partition). Get this value from
+# ./get-disk-info.sh:
+wwn="..."
 
-wwn="..." # if this is set, do not set $image_file
-image_file="..." # if this is set, do not set $wwn
-lvm_name="foo"
+# Give a name to your LVM volumes. This is usually same as your
+# installation name (eg. mysystem)
+lvm_name="mysystem"
 
-# use ./get-disk-info.sh to identify the UUID's:
-boot_part='UUID=4371a575-3a37-476f-8831-05558db4e8a8'
-crypt_part='UUID=bf1a7669-e944-444d-83cc-102f34689544'
+# Assign below variables *after* partitioning the disk
+# (format-btrfs-swap-lvm-luks.sh... step)
+# use ./get-disk-info.sh /dev/sdX again to identify the UUID's:
+boot_part='UUID=...'
+crypt_part='UUID=....'
 
 # you probably won't need to change those:
 crypt_dev_name=${lvm_name}_crypt
