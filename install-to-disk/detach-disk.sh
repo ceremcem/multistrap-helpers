a3d1d85f7a28bc84bad0500247973962c7d74153
Cerem Cem ASLAN <cem@aktos.io>
2021-01-02 20:28:29 +0300

    detach-disk now forcefully removes the lvm devices when necessary
    When the disk is first removed physically, lvchange -a n ... fails
    to remove the LVM volume groups. `dmsetup remove $device` is
    introduced to overcome the issue in such cases.

--- install-to-disk/detach-disk.sh
+++ install-to-disk/detach-disk.sh
@@ -8,6 +8,11 @@ function echo_and_run {
   eval $(printf '%q ' "$@") < /dev/tty
 }
 
+lvm_open_count(){
+    local device=$1
+    dmsetup info $device | grep "Open count" | cut -d: -f2 | tr -d ' '
+}
+
 config_file=${1:-}
 [[ ! -f $config_file ]] && { echo "Usage: $(basename $0) path/to/config-file"; exit 1; }
 safe_source $config_file
@@ -19,8 +24,16 @@ if sudo btrfs scrub cancel $root_mnt 2> /dev/null; then
     echo "Cancelling BTRFS scrub."
 fi
 
-echo_and_run umount $root_mnt || true
-while read dev; do
+if mountpoint $root_mnt > /dev/null; then
+    echo "Unmounting $root_mnt"
+    umount $root_mnt
+else
+    echo "INFO: $root_mnt is not mounted."
+fi 
+
+while read _dev; do
+    [[ -z $_dev ]] && continue
+    dev="${lvm_name}-${_dev}"
     device="/dev/mapper/$dev"
 	if [[ ! -z `cat /proc/mounts | grep "$dev"` ]]; then
 		echo "$dev seems to be mounted, unmounting first."
@@ -33,14 +46,13 @@ while read dev; do
             echo "(see https://stackoverflow.com/a/58121313/1952991)"
         fi
 	fi
-	echo "deactivating $device"
-	lvchange -a n $device
-done < <(lvs --noheadings -o active,vg_name,lv_name | \
-	grep $lvm_name | \
- 	awk '$1 == "active" {print $2"-"$3}')
+	echo "Deactivating LVM volume: $device"
+	lvchange -a n $device || dmsetup remove $device
+done <<< $(ls /dev/$lvm_name 2> /dev/null) 
 
-echo "Closing $crypt_dev_name"
-cryptsetup close $crypt_dev_name || true
+
+echo -n "Closing $crypt_dev_name: "
+cryptsetup close $crypt_dev_name && echo "OK" || exit 1; 
 
 echo "Removing $root_mnt directory"
 rmdir "$root_mnt" 2> /dev/null || true
