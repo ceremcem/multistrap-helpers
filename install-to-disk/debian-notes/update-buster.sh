6b0fb5c760b4a24d0dbeb4ecf5432bf66f15794f
Cerem Cem ASLAN <cem@aktos.io>
2021-01-27 00:34:18 +0300

    added update script

---
+++ install-to-disk/debian-notes/update-buster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+[[ "$(whoami)" == "root" ]] || { sudo $0 "$@"; exit 0; }
+
+set -x
+apt-get update
+
+sudo service vboxdrv stop
+sudo apt-get upgrade
+
+set +x
+echo "Upgrading packages manually installed from backports:"
+aptitude search "?narrow(~i, ~Abackports) ?not(?automatic)" \
+    | awk '{print $2}' \
+    | xargs apt-get -t buster-backports install
+
+set -x
+sudo apt-get dist-upgrade
+
+/sbin/vboxconfig
