9aa40832cbcc3260abeac56b3dfc35c50c1e782d
Cerem Cem ASLAN <cem@aktos.io>
2021-01-27 19:08:07 +0300

    create-vmdk handles config files from outside THIS directory

--- install-to-disk/create-vmdk.sh
+++ install-to-disk/create-vmdk.sh
@@ -3,18 +3,26 @@ set -eu
 
 safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; _sdir=$(dirname "$(readlink -f "$0")"); }; safe_source
 
-if [[ "${1:-}" == "-c" ]]; then 
-	config=$2
-	cd "$(dirname "$config")"
-	source $config
-	[[ -n ${wwn:-} ]] && { $0 "--disk" $wwn; exit 0; }
-	[[ -n ${image_file:-} ]] && { $0 "--file" $image_file; exit 0; }
+die(){ echo "$@"; exit 1; }
+
+if [[ "${1:-}" == "-c" ]]; then
+	shift  
+	config_file=${1:-}
+	[[ -n $config_file && -f $config_file ]] \
+	    && config_file=$(realpath $config_file) \
+	    || die "Usage: $(basename $0) -c path/to/config-file"
+	. $config_file
+
+	this=$(realpath $0)
+	cd "$(dirname $config_file)" # create vmdk file in the same directory of config file.
+	[[ -n ${wwn:-} ]] && { $this "--disk" $wwn; exit 0; }
+	[[ -n ${image_file:-} ]] && { $this "--file" "$image_file"; exit 0; }
 	echo "Something went wrong. You should set \$wwn or \$image_file."
 	exit 1
 fi
 
 # hack for first arguments
-shift 
+[[ -n ${1:-} ]] && shift 
 
 if [[ -b ${1:-} ]]; then
 	device=$(readlink -f $1)
@@ -24,14 +32,9 @@ if [[ -b ${1:-} ]]; then
 elif [[ -f ${1:-} ]]; then
 	file=$1
 	vmdk_name="$(basename $file).vmdk"
+	echo "pwd is: $PWD"
 else
-	echo
-	echo "Usage: $(basename $0) -c path/to/config.sh"
-	echo "Usage: $(basename $0) --disk /dev/sdX"
-	echo "Usage: $(basename $0) --disk /dev/disk/by-id/disk-wwn"
-	echo "Usage: $(basename $0) --file /path/to/disk.img"
-	echo
-	exit 1
+	die "Usage: $(basename $0) -c path/to/config-file"
 fi
 
 #[[ $(whoami) = "root" ]] || { sudo "$0" "$@"; exit 0; }
@@ -42,7 +45,7 @@ _device=${disk_path:-$file}
 vmdk_name="virtualbox-$vmdk_name"
 echo "Creating vmdk for ${_device}"
 VBoxManage internalcommands createrawvmdk \
-    -filename $_sdir/$vmdk_name \
+    -filename ./$vmdk_name \
     -rawdisk ${_device}
 
 #chown $SUDO_USER:$SUDO_USER "$_sdir/$vmdk_name"
