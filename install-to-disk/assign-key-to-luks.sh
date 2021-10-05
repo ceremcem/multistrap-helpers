baac1eb42f0e327c860ed35f822d25670865b4ee
Cerem Cem ASLAN <cem@aktos.io>
2021-10-05 12:00:53 +0300

    improved error handling and console messages

--- install-to-disk/assign-key-to-luks.sh
+++ install-to-disk/assign-key-to-luks.sh
@@ -2,12 +2,20 @@
 set -eu
 safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; _sdir=$(dirname "$(readlink -f "$0")"); }; safe_source
 
-config_file=${1:-}
+config_file="$(realpath ${1:-})"
 [[ ! -f $config_file ]] && { echo "Usage: $(basename $0) path/to/config-file"; exit 1; }
 cd "$(dirname "$config_file")"
 safe_source $config_file
 
 [[ -z ${crypt_key:-} ]] && { echo "You should define \$crypt_key variable in your config file."; exit 2; }
+
+if ! [[ -f $crypt_key ]]; then
+    echo "You should generate a random file to use as a key. Example:"
+    echo
+    echo "       dd if=/dev/urandom of=${crypt_key} count=1000"
+    echo
+    exit 1
+fi
 
 if sudo cryptsetup open $crypt_part --test-passphrase --key-file=$crypt_key; then
     echo "This key ($crypt_key) is already assigned."
