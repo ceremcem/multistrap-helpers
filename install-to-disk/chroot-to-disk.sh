35db9ec37ea4155c6e5a36ad41efc91cb466f2b5
Cerem Cem ASLAN <cem@aktos.io>
2021-10-08 20:09:19 +0300

    fixed additional argument passing

--- install-to-disk/chroot-to-disk.sh
+++ install-to-disk/chroot-to-disk.sh
@@ -9,14 +9,13 @@ die(){
 
 [[ $(whoami) = "root" ]] || die "This script must be run as root."
 config_file=${1:-}
+shift
 [[ -n $config_file && -f $config_file ]] \
     && config_file=$(realpath $config_file) \
     || die "Usage: $(basename $0) path/to/config-file" 
 cd "$(dirname "$config_file")"
 . $config_file
 
-shift
-args=("${@:-}")
 
 # create temporary directory for assembly: see https://unix.stackexchange.com/q/558604/65781
 tmp="${root_mnt}-${subvol}.chroot"
@@ -27,7 +26,7 @@ echo "Mounting $subvol (from $root_dev/) on $tmp"
 mount $root_dev $tmp -o rw,subvol=$subvol,noatime
 echo "Mounting $boot_part to $tmp/boot"
 mount $boot_part $tmp/boot
-$_sdir/../do-chroot.sh $tmp "$args"
+$_sdir/../do-chroot.sh $tmp "$@"
 set -x
 umount $tmp/boot
 umount $tmp
